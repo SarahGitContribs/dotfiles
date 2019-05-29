91700115fc7676f1b0ba960e1a841252a453973d
SarahGitContribs <50883527+SarahGitContribs@users.noreply.github.com>
2019-05-28 21:34:36 -0400

    More modular install script

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-rc=config/openbox/rc.xml
-autostart=config/openbox/autostart
-compton=config/compton.conf
-if [ ! -d ~/.config/openbox ]; then 
-	mkdir ~/.config/openbox
+#!/bin/sh
+directory () { 
+if [ ! -d ~/.$1 ]; then 
+	mkdir ~/.$1
 fi
+}
 install () {
 if [ -e ~/.$1 ]; then 
 	read -p "Backup and replace ~/.$1 (y/n)?" option
 	if [ "$option" = "y" ]; then
-		mv ~/.$1 ~/.$1.backup
+		mv ~/.$1 ~/.$1.bak
 		mv $1 ~/.$1
 	fi
 	else
 		mv $1 ~/.$1
 fi
-} 
-install $rc
-install $autostart
-install $compton
+}
+directory config/openbox
+install config/openbox/rc.xml
+install config/openbox/autostart
+install config/compton.conf
